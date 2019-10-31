861a55a6680857bdb1f7db5b20b8e0e593f721e6
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-3.local>
2019-10-31 17:35:14 -0400

    Add model properties

--- src/main/java/models/Appliances.java
+++ src/main/java/models/Appliances.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Appliances {
+
+    private int id;
+    private String name;
+    private String brand;
+    private String type;
+    private int size;
+    private int qty;
+    private float price;
 }
+
--- src/main/java/models/Furniture.java
+++ src/main/java/models/Furniture.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Furniture {
+    private int id;
+    private String name;
+    private String brand;
+    private String type;
+    private int size;
+    private int qty;
+    private float price;
 }
